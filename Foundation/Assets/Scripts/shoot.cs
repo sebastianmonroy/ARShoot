66afe0ea05fe54e35ab02606f913ed9de4721379
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-02-11 19:40:42 -0500

    Raycasting, 3 Random Decals, 2 Random Models

---
+++ Foundation/Assets/Scripts/shoot.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+	public GameObject[] decalPrefabs;
+	//public GameObject pinata;
+	//public GameObject bullet;
+	//public float speed;
+	private float waitToShoot;
+	public float waitDuration;
+
+	// Use this for initialization
+	void Start () {
+		//pinata = GameObject.FindWithTag("pinata");
+		waitToShoot = waitDuration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.touchCount > 0 || Input.GetMouseButtonDown(0)) && waitToShoot <= 0) {
+
+			RaycastHit hit = new RaycastHit();
+			if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+				if (hit.collider.gameObject.tag == "Pinata") {
+					int decalIndex = Random.Range(0,decalPrefabs.Length);
+					GameObject decal = Instantiate(decalPrefabs[decalIndex], hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal)) as GameObject;
+					decal.transform.localScale *= Random.Range(1.0f,3.0f);
+					decal.transform.RotateAround(hit.normal, Random.Range(0.0f, 360.0f));
+				}
+			}
+
+			/*GameObject blah = Instantiate(bullet, transform.position + Vector3.forward * 50, transform.rotation) as GameObject;
+			blah.rigidbody.velocity = transform.forward * speed;*/
+
+			waitToShoot = waitDuration;
+		}
+
+		waitToShoot -= Time.deltaTime;
+	}
+}
---
+++ Foundation/Assets/Scripts/spawn.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawn : MonoBehaviour {
+	public GameObject[] modelPrefabs;
+	public int numModels;
+	public bool onlyModel;
+	private GameObject model;
+	private float modelScale;
+
+	void Start () {
+		// Prevent invalid number of models to choose from
+		if (numModels <= 0) {
+			numModels = modelPrefabs.Length-1;
+		}
+
+		// Prevent invalid model scale
+		if (modelScale <= 0) {
+			modelScale = 1;
+		}
+
+		// If onlyModel is enabled, do not randomly select model
+		int modelIndex;
+		if (onlyModel) {
+			modelIndex = numModels;
+		} else {
+			modelIndex = Random.Range(0,numModels+1);
+		}
+
+		// Instantiate model based on above options
+		model = Instantiate(modelPrefabs[modelIndex], this.transform.position, Random.rotation) as GameObject;
+		//model.transform.position = -1 * model.collider.bounds.center;
+		model.transform.parent = this.transform;
+		// Fixes size of the model based on Spawn size
+		model.transform.localScale *= modelScale;
+		model.renderer.enabled = true;
+	}
+
+	void Update () {
+		// Prevent model from ever being rendered
+		if (model != null && model.renderer.enabled) {
+			//model.renderer.enabled = false;
+		}
+	}
+}
