d81e6b6d9f4cd818790fe51de70aa9e988c7e8a2
Sebastian Monroy <sebastian.monroy@gmail.com>
2014-02-18 13:26:23 -0500

    Cubify Prototype
    Can now generate cubes of user-defined size either surrounding an object
    or within an object.
    
    Note: need to make cube generation in Update() more efficient. Need to
    handle bullet collisions with cubes.

---
+++ Foundation/Assets/Scripts/cube.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cube : MonoBehaviour {
+	private bool deleteUponCubify;
+	public float waitDuration;
+
+	// Use this for initialization
+	void Start () {
+		deleteUponCubify = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		waitDuration -= Time.deltaTime;
+		if (waitDuration < 0) {
+			//deleteUponCubify = false;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		print("cube collision");
+		if (deleteUponCubify && collision.gameObject.tag == "Pinata") {
+			print("cube destroy");
+			Destroy(this.gameObject);
+		}
+	}
+}
